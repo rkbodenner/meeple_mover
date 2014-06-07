7062fab787ba91f419d81ae11c1d4f6a98c9c923
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-06-06 21:42:06 -0700

    Serve game state JSON

--- meeple_mover.go
+++ meeple_mover.go
@@ -1,21 +1,18 @@
 package main
 
 import (
+  "encoding/json"
   "fmt"
   "net/http"
-  "strings"
   "github.com/rkbodenner/parallel_universe/game"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
   game := game.NewGame(nil, 2)
-  players := make([]string, 0)
-  for _,p := range game.Players {
-    players = append(players, (string)(p))
+  err := json.NewEncoder(w).Encode(game)
+  if ( nil != err ) {
+    fmt.Fprintln(w, err)
   }
-  playerText := strings.Join(players, "\n")
-
-  fmt.Fprintf(w, playerText)
 }
 
 func main() {
