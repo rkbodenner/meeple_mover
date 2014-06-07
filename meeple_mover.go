f00bb82ba20fec4bb005df45adf3ef73fc39a075
Ralph Bodenner <rkbodenner+github@gmail.com>
2014-06-07 13:37:07 -0700

    New endpoint for player data, add middleware for CORS in testing

--- meeple_mover.go
+++ meeple_mover.go
@@ -7,10 +7,32 @@ import (
   "github.com/rkbodenner/parallel_universe/game"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  header := w.Header()
-  header.Add("Access-Control-Allow-Origin", "http://localhost:8000")
+type Player struct {
+  Id int
+  Name string
+}
 
+var players = []Player{
+  {1, "Player One"},
+  {2, "Player Two"},
+}
+
+func corsHandler(h http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    header := w.Header()
+    header.Add("Access-Control-Allow-Origin", "http://localhost:8000")
+    h.ServeHTTP(w, r)
+  })
+}
+
+func playersHandler(w http.ResponseWriter, r *http.Request) {
+  err := json.NewEncoder(w).Encode(players)
+  if ( nil != err ) {
+    fmt.Fprintln(w, err)
+  }
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
   game := game.NewGame(nil, 2)
   err := json.NewEncoder(w).Encode(game)
   if ( nil != err ) {
@@ -19,6 +41,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-  http.HandleFunc("/", handler)
+  http.Handle("/", corsHandler(http.HandlerFunc(handler)))
+  http.Handle("/players", corsHandler(http.HandlerFunc(playersHandler)))
   http.ListenAndServe(":8080", nil)
 }
